a0779d1867531094be77847e3e8a409a744ff3a4
KH178 <karanhanda17821@gmail.com>
2020-07-26 01:27:21 +0530

    Updates

---
+++ precache-manifest.a6128240b04fb8985faee6780b3d9bdb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cc1b9455052060df20d5281136d73b",
+    "url": "/Color-App/index.html"
+  },
+  {
+    "revision": "f63c8152c6111b5ec2db",
+    "url": "/Color-App/static/css/2.12f73d73.chunk.css"
+  },
+  {
+    "revision": "aa4235fee41ff1c6d08b",
+    "url": "/Color-App/static/css/main.5a004eff.chunk.css"
+  },
+  {
+    "revision": "f63c8152c6111b5ec2db",
+    "url": "/Color-App/static/js/2.f8b906c6.chunk.js"
+  },
+  {
+    "revision": "f7a6089cb49ce8d56c4f81fbdbd2ab2d",
+    "url": "/Color-App/static/js/2.f8b906c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4235fee41ff1c6d08b",
+    "url": "/Color-App/static/js/main.dbda18a4.chunk.js"
+  },
+  {
+    "revision": "0d897bfb28fb172dce97",
+    "url": "/Color-App/static/js/runtime-main.9ba65c03.js"
+  },
+  {
+    "revision": "543e33d2e2f6821cce057d07a5d73afb",
+    "url": "/Color-App/static/media/bg.543e33d2.svg"
+  }
+]);
